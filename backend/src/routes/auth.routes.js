cc400c336cf79b37cf081d416daf48287d6e35b3
SAVillena <83886170+PynaflowxD@users.noreply.github.com>
2023-10-12 22:56:34 -0300

    template backend

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,18 @@
+"use strict";
+// Importa el modulo 'express' para crear las rutas
+const express = require("express");
+
+/** Controlador de autenticación */
+const authController = require("../controllers/auth.controller.js");
+
+/** Instancia del enrutador */
+const router = express.Router();
+
+// Define las rutas para la autenticación
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.get("/refresh", authController.refresh);
+router.post("/signin", authController.login);
+
+// Exporta el enrutador
+module.exports = router;
