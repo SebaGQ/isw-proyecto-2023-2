0993267d84b367f657812425bda87b424990c784
SAVillena <83886170+PynaflowxD@users.noreply.github.com>
2023-10-25 22:16:26 -0300

    pequeño cambio en schema

--- backend/src/schema/pauta.schema.js
+++ backend/src/schema/pauta.schema.js
@@ -4,12 +4,12 @@ const pautaBodySchema = Joi.object({
     NombrePauta: Joi.string().required().messages({
       "string.empty": "El nombre de la pauta no puede estar vacío.",
       "any.required": "El nombre de la pauta es obligatorio.",
-      "string.base": "El nombre de la pauta debe ser de tipo string."
+      "string.base": "El nombre de la pauta debe ser de tipo string.",
     }),
     MaxPorcentajeFichaHogar: Joi.string().required().messages({
       "string.empty": "El porcentaje maximo de la ficha hogar no puede estar vacío.",
       "any.required": "El porcentaje maximo de la ficha hogar es obligatorio.",
-      "string.base": "El porcentaje maximo de la ficha hogar debe ser de tipo string."
+      "string.base": "El porcentaje maximo de la ficha hogar debe ser de tipo string.",
     }),
     MinCantidadIntegrantes: Joi.number().positive().required().messages({
       "number.base": "La cantidad minima de integrantes debe ser un número.",
--- backend/src/schema/subsidio.schema.js
+++ backend/src/schema/subsidio.schema.js
@@ -4,12 +4,12 @@ const subsidioBodySchema = Joi.object({
   Name: Joi.string().required().messages({
     "string.empty": "El nombre del subsidio no puede estar vacío.",
     "any.required": "El nombre del subsidio es obligatorio.",
-    "string.base": "El nombre del subsidio debe ser de tipo string."
+    "string.base": "El nombre del subsidio debe ser de tipo string.",
   }),
   Descripcion: Joi.string().required().messages({
     "string.empty": "La descripcion del subsidio no puede estar vacío.",
     "any.required": "La descripcion del subsidio es obligatorio.",
-    "string.base": "La descripcion del subsidio debe ser de tipo string."
+    "string.base": "La descripcion del subsidio debe ser de tipo string.",
   }),
   Monto: Joi.number().positive().required().messages({
     "number.base": "El monto debe ser un número.",
