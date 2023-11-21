25181db365ade2cd4a3a3b085f2888240d56ae69
SebaGQ <segutierrez.q@gmail.com>
2023-11-20 23:42:41 -0300

    Pequeños cambios visuales

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,19 +1,8 @@
 import LoginForm from '../components/LoginForm';
-import { useNavigate } from 'react-router-dom';
+
 import '../styles/LoginForm.css';
 
 function Login() {
-  const navigate = useNavigate();
-
-  if (localStorage.getItem('user')) {
-    return (
-      <div className="login-form-container">
-        <h2>Ya estás logueado!</h2>
-        <button onClick={() => navigate('/')}>Ir a home</button>
-      </div>
-    );
-  }
-
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <div style={{ width: '100%', maxWidth: '400px' }}>
