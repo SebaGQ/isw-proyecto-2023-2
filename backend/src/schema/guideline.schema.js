164562a8a5256a050ea763f6e1498b29df00d499
3rr0r-FARC <fabian.arc9217@gmail.com>
2023-12-11 23:19:01 -0400

    agregacion mensaje schema

--- backend/src/schema/guideline.schema.js
+++ backend/src/schema/guideline.schema.js
@@ -25,6 +25,7 @@ const guidelineBodySchema = Joi.object({
       "number.base": "El porcentaje social máximo debe ser de tipo número.",
       "number.min": "El porcentaje social máximo no puede ser menor que 0.",
       "number.max": "El porcentaje social máximo no puede ser mayor que 100.",
+      "number.positive": "El monto debe ser un número positivo.",
       "any.required": "El porcentaje social máximo es obligatorio.",
     }),
   minMembers: Joi.number()
@@ -33,6 +34,7 @@ const guidelineBodySchema = Joi.object({
     .messages({
       "number.base": "El mínimo de miembros debe ser de tipo número.",
       "number.min": "El mínimo de miembros no puede ser menor que 1.",
+      "number.positive": "El monto debe ser un número positivo.",
       "any.required": "El mínimo de miembros es obligatorio.",
     }),
 }).messages({
