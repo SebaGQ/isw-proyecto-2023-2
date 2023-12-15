05edd073ce36e75e7debe11ce74e973be21f50e4
SAVillena <83886170+PynaflowxD@users.noreply.github.com>
2023-12-15 19:48:08 -0300

    Cambio Front General

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,9 +1,37 @@
+import '../styles/App.css';
 function App() {
   return (
     <>
-      <h1>Pagina principal</h1>
-      <p>Ver qué añadir acá</p>
+      <h1>Postulación para Subsidios y Beneficios</h1>
+      <header>
+        <h1>Bienvenidos a la Plataforma de Postulación para Subsidios y Beneficios</h1>
+        <p>Encuentra la ayuda que necesitas, cuando más la necesitas.</p>
+    </header>
+
+    <section>
+        <h2>¿Cómo Postular?</h2>
+        <p>El proceso de postulación es rápido y fácil, siguiendo estos pasos:</p>
+        <ol>
+            <li><strong>Selecciona el Subsidio o Beneficio</strong>: Navega por nuestra lista de programas disponibles y elige el que mejor se adapte a tus necesidades.</li>
+            <li><strong>Completa el Formulario de Postulación</strong>: Proporciona toda la información requerida de manera precisa.</li>
+            <li><strong>Envía tu Postulación</strong>: Revisa tus datos y, si todo está correcto, envía tu solicitud.</li>
+            <li><strong>Revisa el Estado de tu Postulación</strong>: Revisa el estado de tu postulación en la sección "Mis Postulaciones".</li>
+        </ol>
+    </section>
+
+    <section>
+        <h2>Datos Necesarios para Postular</h2>
+        <p>Para asegurar una postulación exitosa, es importante que prepares la siguiente información:</p>
+        <ul>
+
+            <li>Información Familiar: Número de miembros de tu hogar y sus datos básicos.</li>
+            <li>Datos de Ingresos: Porcentaje ficha hogar.</li>
+        </ul>
+    </section>
+
     </>
+
+    
   );
 }
 
