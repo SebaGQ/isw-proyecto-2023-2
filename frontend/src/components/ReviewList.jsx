7ded6a5b43d7ae92d1733564fee8cc2e897c1be6
SAVillena <83886170+PynaflowxD@users.noreply.github.com>
2023-12-15 19:09:22 -0300

    se agrega validacion de estado del backend a front

--- frontend/src/components/ReviewList.jsx
+++ frontend/src/components/ReviewList.jsx
@@ -7,22 +7,21 @@ const ReviewList = ({ reviews, onEditReview }) => {
             {reviews.length > 0 ? (
                 reviews.map(review => (
                     <div key={review._id} className="review-item">
-                        {/* Mostrar información relevante de applicationId */}
-                        <h3 className="review-title">Revisión de la solicitud: {review.applicationId._id}</h3>
-                        <p className="review-applicant">Solicitante: {review.applicationId.userId}</p>
+                        <h3 className="review-title">Solicitante: {review.applicationId.userId}</h3>
                         <p className="review-subsidy">Subsidio: {review.applicationId.subsidyId}</p>
-                        <p className="review-socialPercentage">Porcentaje Social: {review.applicationId.socialPercentage}%</p>
-                        <p className="review-applicationDate">Fecha de Postulación: {new Date(review.applicationId.applicationDate).toLocaleDateString()}</p>
-                        <p className="review-members">Número de Miembros: {review.applicationId.members}</p>
+                        <p className="review-socialPercentage">Porcentaje Social: {review.applicationId.socialPercentage}% {review.statusPercentage ? '✔' : '❌'}</p>
+                        <p className="review-applicationDate">Fecha de Postulación: {new Date(review.applicationId.applicationDate).toLocaleDateString() }{review.statusDate ? ' ✔' : ' ❌'}</p>
+                        <p className="review-members">Número de Miembros: {review.applicationId.members} {review.statusMembers ? '✔' : '❌'}</p>
 
                         {/* Información del review */}
                         <p className="review-comments">Comentarios: {review.comments.join(", ")}</p>
                         <p className="review-status">Estado: {review.status}</p>
                         <p className="review-origin">Origen: {review.origin}</p>
+
                         <button className="edit-button" onClick={() => onEditReview(review)}> Editar</button>
                     </div>
                 ))
-            ) :  (
+            ) : (
                 <p>No hay revisiones disponibles.</p>
             )}
         </div>
