49c06a1fdfd113334e69035d7f5f169a322aacd8
SebaGQ <segutierrez.q@gmail.com>
2023-12-18 11:11:17 -0300

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,8 @@ async function setupServer() {
     server.use(express.json());
     // Agregamos los cors
     // server.use(cors({ origin: "/" }));
-    server.use(cors({ origin: "http://localhost:443" }));
+    server.use(cors({ origin: "http://146.83.198.35:1615" }));
+    
 
 
 
