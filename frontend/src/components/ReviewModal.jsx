dd28ba2c3cd7a4fd3cad63a9bfa978c9feaf0714
SAVillena <83886170+PynaflowxD@users.noreply.github.com>
2023-12-17 22:46:05 -0300

    Review

--- frontend/src/components/ReviewModal.jsx
+++ frontend/src/components/ReviewModal.jsx
@@ -51,7 +51,7 @@ const DetailsModal = ({ isOpen, onClose, application, reviews, loadingReviews })
                                     <td>{review.origin}</td>
                                     <td>{review.status}</td>
                                     <td>{formatDate(review.createdAt)}</td>
-                                    <td>{review.socialPercentage} %</td>
+                                    <td>{review.socialPercentage}</td>
                                     <td>{review.members}</td>
                                 </tr>
                             ))}
